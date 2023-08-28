2c4264e3f28eacf5b0cdd543bd23d48674ee9924
Vectormike <victorjonah199@gmail.com>
2023-08-28 02:56:03 +0100

    chore: add helpers

---
+++ src/helpers/env.ts
@@ -0,0 +1,23 @@
+import * as dotenv from 'dotenv';
+
+// Load .env file based on environment
+if (process.env.NODE_ENV === 'development') dotenv.config({ path: '.env' });
+
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+
+class Env {
+	nodeEnv = this.get('NODE_ENV');
+
+	get(variable: any): any {
+		return process.env[variable];
+	}
+
+	getBackendUrl(): string {
+		if (this.nodeEnv === 'development') {
+			return this.get('BASE_URL_DEV');
+		}
+		return this.get('BASE_URL_URL_PROD');
+	}
+}
+
+export default new Env();
---
+++ src/helpers/index.ts
@@ -0,0 +1,4 @@
+import response from "./response";
+import paginate from "./paginate";
+
+export { response, paginate };
